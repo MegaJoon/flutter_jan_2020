1c3de65d949a7345c3d4781846e3652d0ecbd015
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-01 15:57:41 +0900

    build background image & appbar
    build background image & appbar

--- flutter_jan_2020/lib/0101/solar_system_ui.dart
+++ flutter_jan_2020/lib/0101/solar_system_ui.dart
@@ -8,10 +8,73 @@ class SolarSystemUI extends StatefulWidget {
 }
 
 class _SolarSystemUIState extends State<SolarSystemUI> {
+  // image
+  String _backgroundImage = "assets/0101/image.jpg";
+
+  // text
+  String title = "Solar system";
+
+  // double
+  double padding = 16.0;
+  double radius = 24.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: Stack(
+        children: <Widget>[
+          // background image
+          Positioned.fill(
+            child: Image.asset(
+              _backgroundImage,
+              fit: BoxFit.fill,
+            ),
+          ),
+
+          // appbar
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              top: true,
+              left: true,
+              right: true,
+              child: Container(
+                height: 40.0,
+                child: Row(
+                  children: <Widget>[
+                    // left icon
+                    IconButton(
+                      onPressed: (){
+                        print("on Clicked!!");
+                      },
+
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        size: 16.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+
+                    // text: title
+                    Text(title,
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          
+
+        ],
+      ),
     );
   }
 }
