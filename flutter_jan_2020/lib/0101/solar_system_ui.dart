44c9e0923315001137350c1f5db74b8f807f0a48
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-01 12:19:17 +0900

    add images
    add images

---
+++ flutter_jan_2020/lib/0101/solar_system_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/9177633-Solar-system-UI
+
+class SolarSystemUI extends StatefulWidget {
+  @override
+  _SolarSystemUIState createState() => _SolarSystemUIState();
+}
+
+class _SolarSystemUIState extends State<SolarSystemUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
