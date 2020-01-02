c6a1b93109bb8b3c45f62ded495ccf4d92b46e99
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-02 13:41:42 +0900

    guitar app - 01.02
    guitar app - 01.02

--- flutter_jan_2020/lib/0102/guitar_app.dart
+++ flutter_jan_2020/lib/0102/guitar_app.dart
@@ -12,13 +12,16 @@ class GuitarApp extends StatefulWidget {
 class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
   // color
   Color _backgroundColor = Color.fromRGBO(236, 226, 209, 1);
+  Color _bottomColor = Color.fromRGBO(229, 220, 204, 1);
 
   // double
   double padding = 16.0;
 
   // string
   String title = "product detail";
+  String description = "The Fender American Elite Stratocaster takes the classic Strat platform, arguably the most ubiquitous and innovative instrument of its kind since its introduction in 1954, and dresses it up with a serious list of upgrades and new appointments. Improvements include 4th-generation Noiseless pickups, refined bridge and tremolo system, and a compound maple neck with new heel contour. 10 possible sounds come from the S-1 switching system atop the volume control for a range of sounds that make the American Elite Strat somehow more versatile. As usual, these Strats are available in a range of finishes and with your choice of maple, rosewood,";
   String _image = "assets/0102/guitar.png";
+  String _image1 = "https://cdn.pixabay.com/photo/2015/07/28/22/03/fireworks-865104__340.jpg";
 
   // bool
   bool isClicked = false;
@@ -48,12 +51,14 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
   @override
   void initState() {
     // transform animation
-    _transformAnimationController = AnimationController(duration: Duration(seconds: 2), vsync: this);
+    _transformAnimationController = AnimationController(duration: Duration(milliseconds: 1500), vsync: this);
 //    ..forward();
 
     transformAnimation = Tween<double>(begin: 0.0, end: pi /2 - 0.01).animate(
       CurvedAnimation(parent: _transformAnimationController,
-      curve: Curves.fastLinearToSlowEaseIn),
+      curve: Curves.fastLinearToSlowEaseIn,
+      reverseCurve: Curves.fastOutSlowIn,
+      ),
     )
     ..addListener((){
       setState(() {
@@ -70,12 +75,15 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
 //    });
 
     // slide up animation
-    _slideAnimationController = AnimationController(duration: Duration(seconds: 2), vsync: this);
+    _slideAnimationController = AnimationController(duration: Duration(milliseconds: 1500), vsync: this);
 //      ..forward();
 
     slideAnimation = Tween<double>(begin: 0.0, end: 500.0).animate(
-      CurvedAnimation(parent: _slideAnimationController,
-          curve: Curves.fastLinearToSlowEaseIn),
+      CurvedAnimation(
+        parent: _slideAnimationController,
+        curve: Curves.fastLinearToSlowEaseIn,
+        reverseCurve: Curves.fastOutSlowIn,
+      ),
     )
       ..addListener((){
         setState(() {
@@ -107,7 +115,7 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      backgroundColor: _backgroundColor,
+      backgroundColor: isClicked? Colors.grey[300].withOpacity(0.80) : _backgroundColor,
       body: Stack(
         children: <Widget>[
           // top transform page
@@ -128,7 +136,7 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
                 ..rotateX(-transformAnimation.value),
                 alignment: FractionalOffset.center,
                 child: AnimatedContainer(
-                  duration: Duration(seconds: 3),
+                  duration: Duration(seconds: 2),
                   curve: Curves.fastLinearToSlowEaseIn,
                   color: isClicked? Colors.white : _backgroundColor,
                   child: Stack(
@@ -172,10 +180,58 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
             top: screenHeight - slideAnimation.value * 1.17,
             left: 0,
             right: 0,
-            height: screenHeight,
+            height: screenHeight - 98.0,
             child: Container(
-              color: Colors.amber,
-              child: Placeholder(),
+              padding: EdgeInsets.only(
+                top: 150.0,
+                left: padding,
+                right: padding,
+                bottom: padding,
+              ),
+              color: _bottomColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  // text: ~~~~~
+                  Text(description,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+
+                  Spacer(),
+
+                  // image
+                  Container(
+                    height: 200.0,
+                    child: Stack(
+                      children: <Widget>[
+                        // image
+                        Positioned.fill(
+                            child: Image.network(
+                              _image1,
+                              fit: BoxFit.fill,
+                            ),
+                        ),
+
+                        // center icon
+                        Positioned.fill(
+                            child: Center(
+                              child: Icon(
+                                Icons.arrow_right,
+                                size: 60.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
 
@@ -196,7 +252,7 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
                   children: <Widget>[
                     // left
                     Icon(
-                      Icons.menu,
+                      isClicked? Icons.close : Icons.menu,
                       size: 28.0,
                       color: Colors.black,
                     ),
@@ -227,7 +283,7 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
             left: padding,
             right: padding,
             child: Container(
-              height: 100.0,
+              height: 110.0,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
