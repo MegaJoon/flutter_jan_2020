8e5e712f27c8d275c370907c04c1c50a67910a20
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-02 12:48:04 +0900

    build top transform page
    build top transform page

--- flutter_jan_2020/lib/0102/guitar_app.dart
+++ flutter_jan_2020/lib/0102/guitar_app.dart
@@ -13,6 +13,16 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
   // color
   Color _backgroundColor = Color.fromRGBO(236, 226, 209, 1);
 
+  // double
+  double padding = 16.0;
+
+  // string
+  String title = "product detail";
+  String _image = "assets/0102/guitar.png";
+
+  // bool
+  bool isClicked = false;
+
   // transform animation
   AnimationController _transformAnimationController;
   Animation<double> transformAnimation;
@@ -21,11 +31,25 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
   AnimationController _slideAnimationController;
   Animation<double> slideAnimation;
 
+  // click event : animation forward & reverse
+  void _onClicked(){
+    setState(() {
+      isClicked = !isClicked;
+      if(isClicked){
+        _transformAnimationController.forward();
+        _slideAnimationController.forward();
+      } else{
+        _transformAnimationController.reverse();
+        _slideAnimationController.reverse();
+      }
+    });
+  }
+
   @override
   void initState() {
     // transform animation
-    _transformAnimationController = AnimationController(duration: Duration(seconds: 2), vsync: this)
-    ..forward();
+    _transformAnimationController = AnimationController(duration: Duration(seconds: 2), vsync: this);
+//    ..forward();
 
     transformAnimation = Tween<double>(begin: 0.0, end: pi /2 - 0.01).animate(
       CurvedAnimation(parent: _transformAnimationController,
@@ -34,20 +58,20 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
     ..addListener((){
       setState(() {
       });
-    })
-    ..addStatusListener((status){
-      setState(() {
-        if(status == AnimationStatus.completed){
-          _transformAnimationController.reverse();
-        } else if(status == AnimationStatus.dismissed){
-          _transformAnimationController.forward();
-        }
-      });
     });
+//    ..addStatusListener((status){
+//      setState(() {
+//        if(status == AnimationStatus.completed){
+//          _transformAnimationController.reverse();
+//        } else if(status == AnimationStatus.dismissed){
+//          _transformAnimationController.forward();
+//        }
+//      });
+//    });
 
     // slide up animation
-    _slideAnimationController = AnimationController(duration: Duration(seconds: 2), vsync: this)
-      ..forward();
+    _slideAnimationController = AnimationController(duration: Duration(seconds: 2), vsync: this);
+//      ..forward();
 
     slideAnimation = Tween<double>(begin: 0.0, end: 500.0).animate(
       CurvedAnimation(parent: _slideAnimationController,
@@ -56,16 +80,16 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
       ..addListener((){
         setState(() {
         });
-      })
-      ..addStatusListener((status){
-        setState(() {
-          if(status == AnimationStatus.completed){
-            _slideAnimationController.reverse();
-          } else if(status == AnimationStatus.dismissed){
-            _slideAnimationController.forward();
-          }
-        });
       });
+//      ..addStatusListener((status){
+//        setState(() {
+//          if(status == AnimationStatus.completed){
+//            _slideAnimationController.reverse();
+//          } else if(status == AnimationStatus.dismissed){
+//            _slideAnimationController.forward();
+//          }
+//        });
+//      });
 
     super.initState();
   }
@@ -92,14 +116,53 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
             left: 0,
             right: 0,
             height: screenHeight,
-            child: Transform(
-              transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.001)
-              ..rotateX(-transformAnimation.value),
-              alignment: FractionalOffset.center,
-              child: Container(
-                color: Colors.teal,
-                child: Placeholder(),
+            child: InkWell(
+              onTap: (){
+                setState(() {
+                  _onClicked();
+                });
+              },
+              child: Transform(
+                transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateX(-transformAnimation.value),
+                alignment: FractionalOffset.center,
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 3),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  color: isClicked? Colors.white : _backgroundColor,
+                  child: Stack(
+                    children: <Widget>[
+                      // text: Fender
+                      Positioned(
+                        top: 200.0,
+                        left: -padding *5.0,
+                        child: Transform.rotate(
+                          angle: 1.56,
+                          child: Text("Fender".toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 90.0,
+                              color: Colors.grey[600].withOpacity(0.50),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      // center image
+                      Positioned(
+                        top: 80.0,
+                          left: 80.0,
+                          right: 80.0,
+                          bottom: 200.0,
+                          child: Image.asset(
+                            _image,
+                            fit: BoxFit.cover,
+                          ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
@@ -115,6 +178,51 @@ class _GuitarAppState extends State<GuitarApp> with TickerProviderStateMixin {
               child: Placeholder(),
             ),
           ),
+
+          // appbar
+          Positioned(
+            top: 0,
+            left: padding,
+            right: padding,
+            child: SafeArea(
+              top: true,
+              left: true,
+              right: true,
+              child: Container(
+                height: 40.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    // left
+                    Icon(
+                      Icons.menu,
+                      size: 28.0,
+                      color: Colors.black,
+                    ),
+
+                    // center text: title
+                    Text(title.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    // spacer
+                    Container(
+                      width: 28.0,
+//                      child: Placeholder(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          // bottom text
+
         ],
       ),
     );
