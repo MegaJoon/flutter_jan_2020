94206a83e16cd05cbc44102e74cad5f6bbe557b0
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-10 01:25:05 +0900

    build bottom bar
    build bottom bar

--- flutter_jan_2020/lib/0110/second_lighting_app.dart
+++ flutter_jan_2020/lib/0110/second_lighting_app.dart
@@ -10,12 +10,15 @@ class SecondLightingApp extends StatefulWidget {
 class _SecondLightingAppState extends State<SecondLightingApp> {
   // color
   Color _backgroundColor = Color.fromRGBO(69, 11, 254, 1);
+  Color _startColor = Color.fromRGBO(247, 155, 145, 1);
+  Color _endColor = Color.fromRGBO(252, 136, 197, 1);
 
   // string
   String title = "Lighting";
 
   // double
   double padding = 16.0;
+  double radius = 16.0;
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +75,108 @@ class _SecondLightingAppState extends State<SecondLightingApp> {
           // bottom bar
           Container(
             height: 80.0,
-            child: Placeholder(),
+            child: Stack(
+              children: <Widget>[
+                // bottom bar
+                Positioned(
+                  top: padding,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(radius),
+                        topRight: Radius.circular(radius),
+                      ),
+                      color: Colors.white,
+                      boxShadow: [BoxShadow(
+                        color: Colors.black12,
+                        spreadRadius: 2.0,
+                        blurRadius: 2.0,
+                      )],
+                    ),
+                  ),
+                ),
+
+                // bottom items
+                Positioned.fill(
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: Center(
+                            child: Icon(
+                              Icons.keyboard,
+                              size: 24.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+
+                        // circle items
+                        Flexible(
+                          fit: FlexFit.tight,
+                          child: Container(
+                            padding: EdgeInsets.all(4.0),
+                            margin: EdgeInsets.only(bottom: 8.0),
+                            height: 60.0,
+                            width: 60.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                gradient: LinearGradient(
+                                  colors: [_startColor, _endColor],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                ),
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        Flexible(
+                          child: Center(
+                            child: Icon(
+                              Icons.favorite,
+                              size: 24.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Center(
+                            child: Icon(
+                              Icons.compare_arrows,
+                              size: 24.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Center(
+                            child: Icon(
+                              Icons.account_circle,
+                              size: 24.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
