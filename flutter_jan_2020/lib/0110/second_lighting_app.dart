87ba499fc9630ef787ad847299d3847c32135714
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-10 01:14:12 +0900

    build appbar
    build appbar

--- flutter_jan_2020/lib/0110/second_lighting_app.dart
+++ flutter_jan_2020/lib/0110/second_lighting_app.dart
@@ -8,10 +8,74 @@ class SecondLightingApp extends StatefulWidget {
 }
 
 class _SecondLightingAppState extends State<SecondLightingApp> {
+  // color
+  Color _backgroundColor = Color.fromRGBO(69, 11, 254, 1);
+
+  // string
+  String title = "Lighting";
+
+  // double
+  double padding = 16.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      // set background color
+      backgroundColor: _backgroundColor,
+
+      // body
+      body: Column(
+        children: <Widget>[
+          // appbar
+          SafeArea(
+            top: true,
+            left: true,
+            right: true,
+            child: Container(
+              margin: EdgeInsets.only(left: padding),
+              height: 40.0,
+              child: Row(
+                children: <Widget>[
+                  // text: title
+                  Text(title,
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  // spacer
+                  Spacer(),
+
+                  // icon: setting
+                  IconButton(
+                    onPressed: (){},
+                    icon: Icon(
+                      Icons.settings,
+                      size: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+
+          // main
+          Flexible(
+            fit: FlexFit.tight,
+            child: Placeholder(),
+          ),
+
+          // bottom bar
+          Container(
+            height: 80.0,
+            child: Placeholder(),
+          ),
+        ],
+      ),
     );
   }
 }
