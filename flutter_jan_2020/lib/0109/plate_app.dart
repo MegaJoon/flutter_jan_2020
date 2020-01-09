d23b459a2ebf5fca158eee06d17dc65cc4364998
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-09 20:20:48 +0900

    build add to cart container
    build add to cart container

--- flutter_jan_2020/lib/0109/plate_app.dart
+++ flutter_jan_2020/lib/0109/plate_app.dart
@@ -11,6 +11,13 @@ class _PlateAppState extends State<PlateApp> {
   // string
   String _bottomImage = "assets/0109/image3.png";
 
+  // double
+  double padding = 16.0;
+  double radius = 8.0;
+
+  double swipeDirection = 0.0;
+  double positionY = -500.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +26,6 @@ class _PlateAppState extends State<PlateApp> {
           // background top list view
 
           // main page view
-
-          // add to cart container
 
           // bottom image
           Positioned(
@@ -31,6 +36,83 @@ class _PlateAppState extends State<PlateApp> {
             child: Image.asset(
               _bottomImage,
               fit: BoxFit.fill,
+            ),
+          ),
+
+          // add to cart container
+          Positioned(
+            left: padding,
+            right: padding,
+            bottom: 80.0,
+            child: Container(
+              height: 48.0 + 64.0,
+              child: Column(
+                children: <Widget>[
+                  // add to cart
+                  Container(
+                    height: 48.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(radius),
+                      color: Colors.black,
+                    ),
+                    child: Center(
+                      child: Text("add to cart",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // swipe to see recipe
+                  GestureDetector(
+                    onVerticalDragStart: (DragStartDetails dragDetails){},
+                    onVerticalDragUpdate: (DragUpdateDetails dragDetails){
+                      setState(() {
+                        swipeDirection = dragDetails.delta.direction;
+
+//                        print("swipeDirection = $swipeDirection");
+
+                        if(swipeDirection > 0){
+                          print("swipe up");
+                          positionY = -500.0;
+                        } else {
+                          print("swipe down");
+                          positionY = 0.0;
+                        }
+                      });
+                    },
+                    onVerticalDragEnd: (DragEndDetails dragDetails){},
+                    child: Container(
+                      height: 64.0,
+                      child: Center(
+                        child: Text("Swipe To See Recipe",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // recipe page
+          AnimatedPositioned(
+            duration: Duration(seconds: 2),
+            curve: Curves.fastLinearToSlowEaseIn,
+            left: 0,
+            right: 0,
+            bottom: positionY,
+            child: Container(
+              height: 500.0,
+              child: Placeholder(),
             ),
           ),
         ],
