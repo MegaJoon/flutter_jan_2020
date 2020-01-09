5777c2f326517f8e43055042daedc3b37618782b
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-10 01:09:50 +0900

    lighting app - one more
    lighting app - one more

---
+++ flutter_jan_2020/lib/0110/second_lighting_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://www.instagram.com/p/B6vBB7DHAkJ/
+
+class SecondLightingApp extends StatefulWidget {
+  @override
+  _SecondLightingAppState createState() => _SecondLightingAppState();
+}
+
+class _SecondLightingAppState extends State<SecondLightingApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
--- flutter_jan_2020/lib/main.dart
+++ flutter_jan_2020/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_jan_2020/0106/lighting_app.dart';
 import 'package:flutter_jan_2020/0107/bank_cards_app.dart';
 import 'package:flutter_jan_2020/0108/customized_option.dart';
 import 'package:flutter_jan_2020/0109/plate_app.dart';
+import 'package:flutter_jan_2020/0110/second_lighting_app.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
 //      home: LightingApp(),  // 01-06
 //      home: BankCardsApp(),  // 01-07
 //      home: CustomizedOption(),  // 01-08
-      home: PlateApp(),  // 01-09
+//      home: PlateApp(),  // 01-09
+      home: SecondLightingApp(),  // 01-10
     );
   }
 }
