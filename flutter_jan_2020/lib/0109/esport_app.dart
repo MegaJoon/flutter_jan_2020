b33790030d3a31fd5c282b3d918d07c8a84bcdde
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-09 10:06:16 +0900

    EsportApp - 01.09
    EsportApp - 01.09

---
+++ flutter_jan_2020/lib/0109/esport_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/9193202--19-Esport-Social-Media-MobileApp-Concept
+
+class EsportApp extends StatefulWidget {
+  @override
+  _EsportAppState createState() => _EsportAppState();
+}
+
+class _EsportAppState extends State<EsportApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
