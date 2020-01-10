cf45c6fd93c7cf6b7c7982cb2a73811c6e483de1
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-10 13:08:44 +0900

    set buildWidgetPages
    set buildWidgetPages

--- flutter_jan_2020/lib/0110/travel_app.dart
+++ flutter_jan_2020/lib/0110/travel_app.dart
@@ -27,6 +27,7 @@ class _TravelAppState extends State<TravelApp> {
 
   @override
   Widget build(BuildContext context) {
+    // build pages widget
     buildWidgetPages();
 
     return Scaffold(
@@ -87,7 +88,87 @@ class _TravelAppState extends State<TravelApp> {
 
   buildWidgetPages(){
     pages = [
-      Placeholder(color: Colors.red),
+      // initial page
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // appbar
+          SafeArea(
+            top: true,
+            left: true,
+            right: true,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: padding),
+              height: 64.0,
+              child: Row(
+                children: <Widget>[
+                  // left title
+                  Text(title,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  // spacer
+                  Spacer(),
+
+                  // right profile image
+                  Container(
+                    height: 32.0,
+                    width: 32.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(_profileImage),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+//                    child: Placeholder(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // text: title
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: padding),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  // line 1
+                  TextSpan(text: subTitle,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  // line 2
+                  TextSpan(text: subTitle1,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // page view
+          Flexible(
+            fit: FlexFit.tight,
+            child: Placeholder(),
+          ),
+        ],
+      ),
+
+      // dont use
       Placeholder(color: Colors.teal),
       Placeholder(color: Colors.pink),
       Placeholder(color: Colors.black),
