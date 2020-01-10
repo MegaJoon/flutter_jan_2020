e70b861d49cb45378d2cc6b79e69e39d5d673f89
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-10 13:26:14 +0900

    build first page - part 1
    build first page - part 1

--- flutter_jan_2020/lib/0110/travel_app.dart
+++ flutter_jan_2020/lib/0110/travel_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_jan_2020/0110/travel_items_list.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // https://www.instagram.com/p/B6yDr42paQv/?igshid=1407brpdumzib
@@ -14,6 +15,7 @@ class _TravelAppState extends State<TravelApp> {
 
   // double
   double padding = 16.0;
+  double radius = 16.0;
 
   // string
   String title = "Hello, Alex";
@@ -24,6 +26,32 @@ class _TravelAppState extends State<TravelApp> {
 
   // bottom bar index
   int currentIndex = 0;
+
+  // page view controller
+  PageController _pageController;
+
+  // page view index
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController(
+        initialPage: selectedIndex,
+        viewportFraction: 0.70,
+    )
+    ..addListener((){
+      setState(() {
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -163,7 +191,116 @@ class _TravelAppState extends State<TravelApp> {
           // page view
           Flexible(
             fit: FlexFit.tight,
-            child: Placeholder(),
+            child: PageView.builder(
+              itemCount: travels.length,
+              scrollDirection: Axis.horizontal,
+              controller: _pageController,
+              onPageChanged: (int _selectedIndex){
+                setState(() {
+                  selectedIndex = _selectedIndex;
+                });
+                },
+              itemBuilder: (context, index){
+                return Container(
+                  padding: EdgeInsets.only(
+                    top: padding,
+                    left: padding,
+                    right: padding,
+                    bottom: padding *2,
+                  ),
+                  margin: EdgeInsets.only(
+                      top: padding *2,
+                      right: padding *2,
+                      bottom: padding *2,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(radius),
+//                    color: Colors.pink,
+                    image: DecorationImage(
+                      image: AssetImage(travels[index].image),
+                      fit: BoxFit.fill,
+                    ),
+                    boxShadow: [BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 2.0,
+                      blurRadius: 2.0,
+                      offset: Offset(2.0, 2.0),
+                    )],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // container: score
+                      Container(
+                        height: 20.0,
+                        width: 48.0,
+                        decoration: ShapeDecoration(
+                          shape: StadiumBorder(),
+                          color: Colors.white.withOpacity(0.20),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            // icon: star
+                            Icon(
+                              Icons.star,
+                              size: 12.0,
+                                color: Colors.white,
+                            ),
+
+                            // text: score
+                            Text(travels[index].score,
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // spacer
+                      Spacer(),
+
+                      // text: title
+                      Text(travels[index].title,
+                        style: TextStyle(
+                          fontSize: 28.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      // text: subTitle
+                      Row(
+                        children: <Widget>[
+                          // icon: location
+                          Icon(
+                            Icons.location_on,
+                            size: 12.0,
+                            color: Colors.white,
+                          ),
+
+                          // spacer
+                          SizedBox(width: 4.0),
+
+                          // text: subtitle
+                          Text(travels[index].subTitle,
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+                },
+            ),
           ),
         ],
       ),
---
+++ flutter_jan_2020/lib/0110/travel_items_list.dart
@@ -0,0 +1,38 @@
+class Travels {
+  String image;
+  String score;
+  String title;
+  String subTitle;
+
+  Travels({this.image, this.score, this.title, this.subTitle});
+}
+
+List<Travels> travels = [
+  Travels(
+    image: "assets/0110/image.jpg",
+    score: "4.5",
+    title: "French",
+    subTitle: "Paris, Tower",
+  ),
+
+  Travels(
+    image: "assets/0110/image1.jpg",
+    score: "4.5",
+    title: "England",
+    subTitle: "The Shard, London",
+  ),
+
+  Travels(
+    image: "assets/0110/image2.jpg",
+    score: "4.5",
+    title: "Japan",
+    subTitle: "Chureta, pagoda",
+  ),
+
+  Travels(
+    image: "assets/0110/image.jpg",
+    score: "4.5",
+    title: "Italy",
+    subTitle: "Ralh-----",
+  ),
+];
