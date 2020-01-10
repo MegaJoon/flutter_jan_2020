c84af1c5e9e52b2fd24ff1e4a67815fcf9d3af0d
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-10 13:05:44 +0900

    build bottom bar
    build bottom bar

--- flutter_jan_2020/lib/0110/travel_app.dart
+++ flutter_jan_2020/lib/0110/travel_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // https://www.instagram.com/p/B6yDr42paQv/?igshid=1407brpdumzib
 
@@ -8,6 +9,9 @@ class TravelApp extends StatefulWidget {
 }
 
 class _TravelAppState extends State<TravelApp> {
+  // list<widget>
+  List<Widget> pages = [];
+
   // double
   double padding = 16.0;
 
@@ -18,83 +22,98 @@ class _TravelAppState extends State<TravelApp> {
   String subTitle = "Where are we\n";
   String subTitle1 = "going today?";
 
+  // bottom bar index
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    buildWidgetPages();
+
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // appbar
-          SafeArea(
-            top: true,
-            left: true,
-            right: true,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: padding),
-              height: 64.0,
-              child: Row(
-                children: <Widget>[
-                  // left title
-                  Text(title,
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
+      body: pages[currentIndex],
 
-                  // spacer
-                  Spacer(),
+      // bottom bar
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
 
-                  // right profile image
-                  Container(
-                    height: 32.0,
-                    width: 32.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: NetworkImage(_profileImage),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-//                    child: Placeholder(),
-                  ),
-                ],
-              ),
-            ),
+        // unselected
+        unselectedIconTheme: IconThemeData(
+          size: 24.0,
+          color: Colors.grey[400],
+        ),
+
+        // selected
+        selectedIconTheme: IconThemeData(
+          size: 24.0,
+          color: Colors.black,
+        ),
+
+        currentIndex: currentIndex,
+
+        // on clicked event
+        onTap: (int _currentIndex){
+          setState(() {
+            currentIndex = _currentIndex;
+          });
+        },
+
+        // bottom items
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.compass),
+            title: Text(""),
           ),
 
-          // text: title
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: padding),
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  // line 1
-                  TextSpan(text: subTitle,
-                      style: TextStyle(
-                        fontSize: 40.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600,
-                      ),
-                  ),
-
-                  // line 2
-                  TextSpan(text: subTitle1,
-                      style: TextStyle(
-                        fontSize: 40.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w300,
-                      ),
-                  ),
-                ],
-              ),
-            ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.search),
+            title: Text(""),
           ),
 
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text(""),
+          ),
 
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            title: Text(""),
+          ),
         ],
       ),
     );
   }
+
+  buildWidgetPages(){
+    pages = [
+      Placeholder(color: Colors.red),
+      Placeholder(color: Colors.teal),
+      Placeholder(color: Colors.pink),
+      Placeholder(color: Colors.black),
+    ];
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
