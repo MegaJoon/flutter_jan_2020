a31b4edc0b4dd0ddf2700ab4ae267ae33da2f9a6
MegaJoon <54565616+MegaJoon@users.noreply.github.com>
2020-01-16 21:03:03 +0900

    text: title, subTitle
    text: title, subTitle

--- flutter_jan_2020/lib/0116/surf_board_app.dart
+++ flutter_jan_2020/lib/0116/surf_board_app.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // https://dribbble.com/shots/9521982-Surfboard-App-Interaction
 
@@ -13,7 +14,14 @@ class _SurfBoardAppState extends State<SurfBoardApp> {
   // color
   Color _color = Color.fromRGBO(117, 63, 238, 1);
   Color _color1 = Color.fromRGBO(106, 62, 203, 1);
+  Color _appbarColor = Color.fromRGBO(151, 222, 254, 1);
 
+  // string
+  String title = "Taylor Swift";
+  String subTitle = "Wen, 21 Aug\n";
+  String subTitle1 = "Club Devil, 5th Avenue, 287";
+
+  // double
   double padding = 16.0;
   double iconPadding = 32.0;
 
@@ -57,7 +65,87 @@ class _SurfBoardAppState extends State<SurfBoardApp> {
                             // spacer
                             Spacer(),
 
+                            // container
+                            Container(
+                              margin: EdgeInsets.only(right: padding),
+                              height: 32.0,
+                              width: 100.0,
+                              decoration: ShapeDecoration(
+                                shape: StadiumBorder(),
+                                color: _appbarColor,
+                                shadows: [BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 1.0,
+                                  blurRadius: 1.0,
+                                  offset: Offset(1.0, 1.0),
+                                )],
+                              ),
+                              child: Center(
+                                child: Text("Purchase",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
 
+                      // text: title
+                      Text(title,
+                        style: GoogleFonts.poppins(
+                          fontSize: 40.0,
+                          color: Colors.amberAccent,
+                          fontWeight: FontWeight.bold,
+                          height: 2.0,
+                          shadows: [BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 6.0,
+                            blurRadius: 6.0,
+                            offset: Offset(6.0, 6.0),
+                          )],
+                        ),
+                      ),
+
+                      // text: subTitle
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          children: [
+                            // text: subTitle
+                            TextSpan(text: subTitle,
+                              style: GoogleFonts.poppins(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                height: 2.0,
+                                shadows: [BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 6.0,
+                                  blurRadius: 6.0,
+                                  offset: Offset(6.0, 6.0),
+                                )],
+                              ),
+                            ),
+
+                            // text: subTitle1
+                            TextSpan(text: subTitle1,
+                              style: GoogleFonts.poppins(
+                                fontSize: 12.0,
+                                color: Colors.grey[500],
+                                fontWeight: FontWeight.w600,
+                                height: 1.5,
+                                shadows: [BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 6.0,
+                                  blurRadius: 6.0,
+                                  offset: Offset(6.0, 6.0),
+                                )],
+                              ),
+                            ),
                           ],
                         ),
                       ),
